42687cfcd784493f962d3273844b084f3c66987a
ZakyMalika <zakymalika0987@gmail.com>
2024-06-04 18:24:59 +0700

    membuat prosedur exception  menggunakan pesan array

--- exception/exception.cpp
+++ exception/exception.cpp
@@ -1,20 +1,19 @@
-// exception.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
+#include <exception>
+// untuk obyek yang akan digunakan
+#include <array>
+// untuk obyek yang akan digunakan
+using namespace std;
 
 int main()
 {
-    std::cout << "Hello World!\n";
+	cout << "Awal PROGRAM" << endl; // PENANDA 1
+	try
+	{
+		array<int, 3> data = { 6, 7, 8 };
+		// pesan array integer 3 element
+		cout << data.at(5) << endl;
+		//memanggil array ke elemnt ke 5
+	}
 }
 
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
