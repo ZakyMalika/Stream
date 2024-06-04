e7f84b3787dfa875ef3c420e74f6d0acc952045f
ZakyMalika <zakymalika0987@gmail.com>
2024-06-04 17:24:04 +0700

    menambahkan setiap karakter dalam satu baris

--- iofile/iofile.cpp
+++ iofile/iofile.cpp
@@ -14,6 +14,13 @@ int main()
 
 	cout << ">= Menuli file, \'q\' untuk keluar" << endl;
 
-
+	// unlimited loop untuk menulis
+	while (true)
+	{
+		cout << "- ";
+			//mendapatkan setiap karakter dalam satu baris
+		getline(cin, baris);
+	}
+	
 
 }
